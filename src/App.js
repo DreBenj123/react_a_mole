188436f9622d904e1b49e30fc2c22c4523411a0d
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-07-29 23:45:28 -0400

    basic skeleton set up

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Mole from "./components/Mole";
+import emptySlot from "./components/emptyslot";
+
+function App() {
+  return;
+}
+
+export default App;
